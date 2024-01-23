7d9870072ee5493d8f2c0c812016576de9b6f4c7
CDferraz <157434735+CDferraz@users.noreply.github.com>
2024-01-22 21:54:44 -0300

    Add files via upload

---
+++ js-curso-2-aula_3/teste.js
@@ -0,0 +1,8 @@
+let listaGenerica = []
+
+
+let linguagemDeProgramação = [Java, C, C++, KOTLIN , PYTHON]
+linguagemDeProgramação.push('Java','Ruby', 'GoLang')
+
+let nomes =['carlos', 'mariana', 'gilberto']
+nomes[1]
